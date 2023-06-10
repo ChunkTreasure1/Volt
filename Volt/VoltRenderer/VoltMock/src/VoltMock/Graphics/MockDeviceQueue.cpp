7f548f3480ff92539a1b68264a277bcae0d1a30e
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2023-06-10 16:36:40 +0200

    added mock device queue

--- Volt/VoltRenderer/VoltMock/src/VoltMock/Graphics/MockDeviceQueue.cpp
+++ Volt/VoltRenderer/VoltMock/src/VoltMock/Graphics/MockDeviceQueue.cpp
@@ -3,5 +3,14 @@
 
 namespace Volt
 {
-
+	void* MockDeviceQueue::GetHandleImpl()
+	{
+		return nullptr;
+	}
+	void MockDeviceQueue::WaitForQueue()
+	{
+	}
+	void MockDeviceQueue::Execute(const std::vector<Ref<CommandBuffer>>& commandBuffer)
+	{
+	}
 }
