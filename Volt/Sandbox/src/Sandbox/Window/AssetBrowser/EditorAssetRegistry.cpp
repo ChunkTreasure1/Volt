e6bbfbcb3dc3896f8136519091fd248fa8026865
Cooltomten <Cooltomten.fr@gmail.com>
2023-06-22 17:20:11 +0200

    fixed the compile errors and added extra info to the mesh asset tooltip

--- Volt/Sandbox/src/Sandbox/Window/AssetBrowser/EditorAssetRegistry.cpp
+++ Volt/Sandbox/src/Sandbox/Window/AssetBrowser/EditorAssetRegistry.cpp
@@ -3,22 +3,27 @@
 #include "EditorAssetRegistry.h"
 #include "Volt/Asset/AssetManager.h"
 #include "Volt/Asset/Mesh/Material.h"
-#define CREATE_ASSET_BROWSER_POPUP_DATA_FUNCTION(type, x) [](Volt::AssetHandle aAssetHandle)->std::vector<std::pair<std::string, std::string>>	\
-			{																																	\
-				auto asset = Volt::AssetManager::GetAsset<type>(aAssetHandle);																	\
-				std::vector<std::pair<std::string, std::string>> data;																			\
-				return data;																													\	
-			}
+
+#define ASSET_BROWSER_POPUP_DATA_FUNCTION_IDENTIFIER(aAssetHandleVarName) [](Volt::AssetHandle aAssetHandleVarName)->std::vector<std::pair<std::string, std::string>>
 
 std::unordered_map<Volt::AssetType, EditorAssetData> EditorAssetRegistry::myAssetData =
 {
 	{
 		Volt::AssetType::Mesh,
 		EditorAssetData(
-			CREATE_ASSET_BROWSER_POPUP_DATA_FUNCTION(Volt::Mesh, apa))
-
-	/*{std::make_pair("Submesh Count", std::to_string(asset->GetSubMeshes().size())))),std::make_pair("Material Path", asset->GetMaterial()->path.string()),std::make_pair("Vertex Count", std::to_string(asset->GetVertexCount())),std::make_pair("Index Count", std::to_string(asset->GetIndexCount()))}*/
-	},
+			ASSET_BROWSER_POPUP_DATA_FUNCTION_IDENTIFIER(aAssetHandle)
+			{
+				auto asset = Volt::AssetManager::GetAsset<Volt::Mesh>(aAssetHandle);
+				std::vector<std::pair<std::string, std::string>> data =
+				{
+					std::make_pair("Submesh Count", std::to_string(asset->GetSubMeshes().size())),
+					std::make_pair("Material Path", asset->GetMaterial()->path.string()),
+					std::make_pair("Vertex Count", std::to_string(asset->GetVertexCount())),
+					std::make_pair("Index Count", std::to_string(asset->GetIndexCount()))
+				};
+				return data;
+			})
+	}
 };
 
 std::vector<std::pair<std::string, std::string>> EditorAssetRegistry::GetAssetBrowserPopupData(Volt::AssetType aAssetType, Volt::AssetHandle aAssetHandle)
