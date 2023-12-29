a474e8bf9a2d542d9ad75228fd7ca90a8756fe94
Ivar Jönsson <chunktreasure@gmail.com>
2023-12-29 23:48:24 +0100

    Implemented base console variables

--- Volt/Volt/src/Volt/Console/ConsoleVariableRegistry.h
+++ Volt/Volt/src/Volt/Console/ConsoleVariableRegistry.h
@@ -15,19 +15,30 @@ namespace Volt
 
 		virtual std::string_view GetName() const = 0;
 		virtual std::string_view GetDescription() const = 0;
+		
+		virtual bool IsInteger() const = 0;
+		virtual bool IsFloat() const = 0;
+		virtual bool IsString() const = 0;
 	};
 
-	template<typename T> 
+	template<typename T>
+	concept ValidConsoleVariableType = std::is_same_v<T, int32_t> || std::is_same_v<T, float> || std::is_same_v<T, std::string>;
+
+	template<ValidConsoleVariableType T>
 	class RegisteredConsoleVariable : public RegisteredConsoleVariableBase
 	{
 	public:
 		RegisteredConsoleVariable(std::string_view variableName, const T& defaultValue, std::string_view description);
 
-		const void* Get() const override;
+		[[nodiscard]] const void* Get() const override;
 		void Set(const void* value) override;
 
-		std::string_view GetName() const override { return m_variableName; }
-		std::string_view GetDescription() const override { return m_description; }
+		[[nodiscard]] inline std::string_view GetName() const override { return m_variableName; }
+		[[nodiscard]] inline std::string_view GetDescription() const override { return m_description; }
+
+		[[nodiscard]] inline constexpr bool IsInteger() const override { return std::is_integral_v<T>; }
+		[[nodiscard]] inline constexpr bool IsFloat() const override { return std::is_floating_point_v<T>; }
+		[[nodiscard]] inline constexpr bool IsString() const override { return std::is_same_v<T, std::string>; }
 
 	private:
 		T m_value;
@@ -35,7 +46,7 @@ namespace Volt
 		std::string_view m_description;
 	};
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	class ConsoleVariable
 	{
 	public:
@@ -58,7 +69,7 @@ namespace Volt
 		Weak<RegisteredConsoleVariable<T>> m_variableReference;
 	};
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	class ConsoleVariableRef
 	{
 	public:
@@ -84,43 +95,53 @@ namespace Volt
 	class ConsoleVariableRegistry
 	{
 	public:
-		template<typename T>
+		template<ValidConsoleVariableType T>
 		static Weak<RegisteredConsoleVariable<T>> RegisterVariable(std::string_view variableName, const T& defaultValue, std::string_view description);
 
-		template<typename T>
+		template<ValidConsoleVariableType T>
 		static Weak<RegisteredConsoleVariable<T>> FindVariable(std::string_view variableName);
+
+		inline static Weak<RegisteredConsoleVariableBase> GetVariable(std::string_view variableName);
+		inline static bool VariableExists(const std::string& variableName);
 
 		static std::unordered_map<std::string_view, Ref<RegisteredConsoleVariableBase>>& GetRegisteredVariables() { return s_registeredVariables; }
 
 	private:
+		ConsoleVariableRegistry() = delete;
+
 		inline static std::unordered_map<std::string_view, Ref<RegisteredConsoleVariableBase>> s_registeredVariables;
 	};
 
-	template<typename T>
+	inline bool ConsoleVariableRegistry::VariableExists(const std::string& variableName)
+	{
+		return s_registeredVariables.contains(std::string_view(variableName));
+	}
+
+	template<ValidConsoleVariableType T>
 	inline RegisteredConsoleVariable<T>::RegisteredConsoleVariable(std::string_view variableName, const T& defaultValue, std::string_view description)
 		: m_value(defaultValue)
 	{
 	}
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline const void* RegisteredConsoleVariable<T>::Get() const
 	{
 		return reinterpret_cast<const void*>(&m_value);
 	}
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline void RegisteredConsoleVariable<T>::Set(const void* value)
 	{
 		m_value = *reinterpret_cast<const T*>(value);
 	}
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline ConsoleVariable<T>::ConsoleVariable(std::string_view variableName, const T& defaultValue, std::string_view description)
 	{
 		m_variableReference = ConsoleVariableRegistry::RegisterVariable<T>(variableName, defaultValue, description);
 	}
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline Weak<RegisteredConsoleVariable<T>> ConsoleVariableRegistry::RegisterVariable(std::string_view variableName, const T& defaultValue, std::string_view description)
 	{
 		Ref<RegisteredConsoleVariable<T>> consoleVariable = CreateRef<RegisteredConsoleVariable<T>>(variableName, defaultValue, description);
@@ -131,7 +152,7 @@ namespace Volt
 		return consoleVariable;
 	}
 	
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline Weak<RegisteredConsoleVariable<T>> ConsoleVariableRegistry::FindVariable(std::string_view variableName)
 	{
 		if (s_registeredVariables.contains(variableName))
@@ -142,10 +163,15 @@ namespace Volt
 		return Weak<RegisteredConsoleVariable<T>>();
 	}
 
-	template<typename T>
+	template<ValidConsoleVariableType T>
 	inline ConsoleVariableRef<T>::ConsoleVariableRef(std::string_view variableName)
 	{
 		m_variableReference = ConsoleVariableRegistry::FindVariable(variableName);
 		VT_CORE_ASSERT(m_variableReference, "Variable with name not found!");
+	}
+
+	inline Weak<RegisteredConsoleVariableBase> ConsoleVariableRegistry::GetVariable(std::string_view variableName)
+	{
+		return s_registeredVariables.at(variableName);
 	}
 }
