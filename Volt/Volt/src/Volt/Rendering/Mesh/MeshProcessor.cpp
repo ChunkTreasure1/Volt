709efbc60e238f2744b2d7cfbfb36a729d78ffec
Ivar Jönsson <chunktreasure@gmail.com>
2024-02-04 19:42:55 +0100

    Add MeshProcessor

---
+++ Volt/Volt/src/Volt/Rendering/Mesh/MeshProcessor.cpp
@@ -0,0 +1,163 @@
+#include "vtpch.h"
+#include "MeshProcessor.h"
+
+#include "Volt/Math/Math.h"
+#include "Volt/Utility/Algorithms.h"
+
+#include <meshoptimizer/meshoptimizer.h>
+
+#include <metis.h>
+
+namespace Volt
+{
+	ProcessedMeshResult MeshProcessor::ProcessMesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices, const MaterialTable& materialTable, const std::vector<SubMesh>& subMeshes)
+	{
+		std::vector<Vertex> resultVertices;
+		std::vector<uint32_t> resultIndices;
+		std::vector<Meshlet> resultMeshlets;
+		std::vector<SubMesh> resultSubMeshes = subMeshes;
+
+		for (auto& subMesh : resultSubMeshes)
+		{
+			std::vector<uint32_t> tempIndices;
+			tempIndices.resize(subMesh.indexCount);
+
+			const uint32_t* indexStartPtr = &indices.at(subMesh.indexStartOffset);
+			const Vertex* vertexStartPtr = &vertices.at(subMesh.vertexStartOffset);
+
+			meshopt_optimizeOverdraw(tempIndices.data(), indexStartPtr, subMesh.indexCount, &vertexStartPtr[0].position.x, subMesh.vertexCount, sizeof(Vertex), 1.05f);
+			
+			auto lod0MeshletData = GenerateMeshlets(vertexStartPtr, indexStartPtr, subMesh.indexCount, subMesh.vertexCount);
+
+			subMesh.meshletStartOffset = static_cast<uint32_t>(resultMeshlets.size());
+			subMesh.meshletIndexStartOffset = static_cast<uint32_t>(resultIndices.size());
+			subMesh.meshletVertexStartOffset = static_cast<uint32_t>(resultVertices.size());
+			subMesh.meshletCount = static_cast<uint32_t>(lod0MeshletData.meshlets.size());
+
+			{
+				// Find meshlets who have adjacent triangles
+				// Find adjacent triangle edge count
+
+				struct Count
+				{
+					uint32_t count = 0;
+				};
+
+				std::vector<std::unordered_set<Edge>> meshletBorderEdges;
+				meshletBorderEdges.resize(lod0MeshletData.meshlets.size());
+
+				for (uint32_t meshletIndex = 0; const auto& meshlet : lod0MeshletData.meshlets)
+				{
+					std::unordered_map<Edge, Count> edgeUsages;
+
+					// A triangle is formed from i+0, i+1, i+2
+					for (uint32_t i = meshlet.triangleOffset; i < meshlet.triangleOffset + meshlet.triangleCount * 3; i += 3)
+					{
+						for (uint32_t j = 0; j < 3; j++)
+						{
+							uint32_t v1 = lod0MeshletData.nonOffsetedIndices.at(i + j);
+							uint32_t v2 = lod0MeshletData.nonOffsetedIndices.at(i + (j + 1) % 3);
+
+							if (v1 > v2)
+							{
+								std::swap(v1, v2);
+							}
+
+							edgeUsages[{ v1, v2 }].count++;
+						}
+					}
+
+					std::unordered_set<Edge>& borderEdges = meshletBorderEdges[meshletIndex];
+					for (const auto& [edge, count] : edgeUsages)
+					{
+						if (count.count == 1)
+						{
+							borderEdges.insert(edge);
+						}
+					}
+
+					meshletIndex++;
+				}
+
+				std::unordered_map<size_t, std::unordered_map<size_t, Count>> meshletAdjecencyCount;
+
+				for (size_t i = 0; i < 1; i++)
+				{
+					const auto& outerEdges = meshletBorderEdges.at(i);
+
+					for (const auto& outerEdge : outerEdges)
+					{
+						for (size_t j = i + 1; j < meshletBorderEdges.size(); j++)
+						{
+							const auto& innerEdges = meshletBorderEdges.at(j);
+							if (innerEdges.contains(outerEdge))
+							{
+								meshletAdjecencyCount[i][j].count++;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return ProcessedMeshResult();
+	}
+
+	MeshletGenerationResult MeshProcessor::GenerateMeshlets(const Vertex* vertices, const uint32_t* indices, uint32_t indexCount, uint32_t vertexCount)
+	{
+		constexpr size_t MAX_VERTEX_COUNT = 64;
+		constexpr size_t MAX_TRIANGLE_COUNT = 64;
+		constexpr float CONE_WEIGHT = 0.f;
+
+		MeshletGenerationResult result{};
+
+		const size_t maxMeshletCount = meshopt_buildMeshletsBound(indexCount, MAX_VERTEX_COUNT, MAX_TRIANGLE_COUNT);
+
+		std::vector<meshopt_Meshlet> tempMeshlets(maxMeshletCount);
+		std::vector<uint32_t> tempVertices(maxMeshletCount * MAX_VERTEX_COUNT);
+		std::vector<uint8_t> tempTriangles(maxMeshletCount * MAX_TRIANGLE_COUNT	* 3);
+
+		const size_t meshletCount = meshopt_buildMeshlets(tempMeshlets.data(), tempVertices.data(), tempTriangles.data(), indices, indexCount,
+			&vertices[0].position.x, vertexCount, sizeof(Vertex), MAX_VERTEX_COUNT, MAX_TRIANGLE_COUNT, CONE_WEIGHT);
+
+		tempMeshlets.resize(meshletCount);
+
+		for (const auto& meshlet : tempMeshlets)
+		{
+			size_t dataOffset = result.meshletData.size();
+
+			for (uint32_t i = 0; i < meshlet.vertex_count; ++i)
+			{
+				result.meshletData.push_back(tempVertices[meshlet.vertex_offset + i]);
+			}
+
+			const uint32_t* indexGroups = reinterpret_cast<const uint32_t*>(tempTriangles.data() + meshlet.triangle_offset);
+			const uint32_t indexGroupCount = (meshlet.triangle_count * 3 + 3) / 4;
+
+			for (uint32_t i = 0; i < indexGroupCount; i++)
+			{
+				result.meshletData.push_back(indexGroups[i]);
+			}
+
+			meshopt_Bounds bounds = meshopt_computeMeshletBounds(&tempVertices[meshlet.vertex_offset], &tempTriangles[meshlet.triangle_offset], static_cast<size_t>(meshlet.triangle_count), &vertices[0].position.x, static_cast<size_t>(vertexCount), sizeof(Vertex));
+		
+			auto& newMeshlet = result.meshlets.emplace_back();
+			newMeshlet.dataOffset = static_cast<uint32_t>(dataOffset);
+			newMeshlet.triangleCount = meshlet.triangle_count;
+			newMeshlet.vertexCount = meshlet.vertex_count;
+			newMeshlet.center = { bounds.center[0], bounds.center[1], bounds.center[2] };
+			newMeshlet.radius = bounds.radius;
+			newMeshlet.coneAxis[0] = bounds.cone_axis_s8[0];
+			newMeshlet.coneAxis[1] = bounds.cone_axis_s8[1];
+			newMeshlet.coneAxis[2] = bounds.cone_axis_s8[2];
+			newMeshlet.coneCutoff = bounds.cone_cutoff_s8;
+
+			for (uint32_t i = meshlet.triangle_offset; i < meshlet.triangle_offset + meshlet.triangle_count * 3; i++)
+			{
+				result.nonOffsetedIndices.push_back(tempVertices[meshlet.vertex_offset + tempTriangles[i]]);
+			}
+		}
+
+		return result;
+	}
+}
