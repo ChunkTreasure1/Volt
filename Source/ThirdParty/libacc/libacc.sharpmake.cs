e1d2333e4653488c03ae9f396e79c19940346420
Ivar Jönsson <chunktreasure@gmail.com>
2024-09-03 21:45:09 +0200

    Added libacc.

---
+++ Source/ThirdParty/libacc/libacc.sharpmake.cs
@@ -0,0 +1,22 @@
+using Sharpmake;
+using System;
+using System.IO;
+
+namespace Volt
+{
+    [Sharpmake.Export]
+    public class libacc : Sharpmake.Project
+    {
+        public libacc() : base(typeof(CommonTarget))
+        {
+            AddTargets(CommonTarget.GetDefaultTargets());
+            Name = "libacc";
+        }
+
+        [Configure()]
+        public void Configure(Configuration conf, CommonTarget target)
+        {
+            conf.IncludePaths.Add(@"[project.RootPath]");
+        }
+    }
+}
