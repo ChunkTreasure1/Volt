0c5943fe53a7e2e95c527e591dab00abac01f5cb
Cooltomten <Cooltomten.fr@gmail.com>
2024-09-03 23:09:47 +0200

    Start moving Sandbox to sharpmake

--- Source/Sandbox/Sandbox.sharpmake.cs
+++ Source/Sandbox/Sandbox.sharpmake.cs
@@ -1,4 +1,5 @@
-using Sharpmake;
+using System;
+using System.IO;
 
 namespace Volt
 {
@@ -7,7 +8,6 @@ namespace Volt
     {
         public Sandbox()
         {
-            AddTargets(CommonTarget.GetDefaultTargets());
             Name = "Sandbox";
         }
 
@@ -15,10 +15,47 @@ namespace Volt
         {
             base.ConfigureAll(conf, target);
 
+            conf.SolutionFolder = "";
+
             conf.PrecompHeader = "sbpch.h";
             conf.PrecompSource = "sbpch.cpp";
 
-            conf.AddPrivateDependency<Volt>(target);
+
+
+            conf.AddPublicDependency<ImGuizmo>(target);
+            conf.AddPublicDependency<imgui_node_editor>(target);
+            conf.AddPublicDependency<p4>(target);
+            conf.AddPublicDependency<OpenSSL>(target);
+
+            conf.IncludePaths.Add(
+                Path.Combine(Globals.ThirdPartyDirectory ,@"nlohmann/include"),
+                Path.Combine(Globals.ThirdPartyDirectory ,@"cpp-httplib/include")
+                );
+
+            conf.AdditionalDebuggerCommands = Path.Combine(Globals.VtProjectDirectory, @"Project.vtproj");
+
+            Console.WriteLine(conf.AdditionalDebuggerCommands);
+
+        }
+
+        public override void ConfigureWin64(Configuration conf, CommonTarget target)
+        {
+            base.ConfigureWin64(conf, target);
+
+            conf.LibraryFiles.Add(
+                "crypt32.lib",
+                "Bcrypt.lib",
+
+                "Winmm.lib",
+                "Version.lib"
+                );
+
+            //we have to add the d3d12 to the exe folder
+            string d3d12FolderPath = Path.Combine(Globals.EngineDirectory, "D3D12");
+            conf.EventPostBuild.Add(@"copy /Y " + "\"" + d3d12FolderPath + "\\D3D12Core.dll\"" + " \"" + conf.TargetPath + "\"");
+            conf.EventPostBuild.Add(@"copy /Y " + "\"" + d3d12FolderPath + "\\D3D12Core.pdb\"" + " \"" + conf.TargetPath + "\"");
+            conf.EventPostBuild.Add(@"copy /Y " + "\"" + d3d12FolderPath + "\\d3d12SDKLayers.dll\"" + " \"" + conf.TargetPath + "\"");
+            conf.EventPostBuild.Add(@"copy /Y " + "\"" + d3d12FolderPath + "\\d3d12SDKLayers.pdb\"" + " \"" + conf.TargetPath + "\"");
         }
     }
 }
---
+++ Source/ThirdParty/OpenSSL/OpenSSL.sharpmake.cs
@@ -0,0 +1,25 @@
+using Sharpmake;
+using System;
+using System.IO;
+
+namespace Volt
+{
+    [Sharpmake.Export]
+    public class OpenSSL : Sharpmake.Project
+    {
+        public OpenSSL() : base(typeof(CommonTarget))
+        {
+            AddTargets(CommonTarget.GetDefaultTargets());
+            Name = "OpenSSL";
+        }
+
+        [Configure()]
+        public void Configure(Configuration conf, CommonTarget target)
+        {
+            conf.IncludePaths.Add(@"[project.RootPath]\include");
+            conf.LibraryPaths.Add(@"[project.RootPath]\lib\" + target.Optimization.ToString());
+
+            conf.Output = Configuration.OutputType.Dll;
+        }
+    }
+}
